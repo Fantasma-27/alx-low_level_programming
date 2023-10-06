2c7b88a821268669539d65b2432fa7640fc73581
fantasma-27 <camimarlyn@gmail.com>
2023-10-06 07:51:29 -0700

    1. The last digit rint the last digit of the number stored in the variable

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+/**
+ * main - Entry point
+ * Description: print alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main() 
+{ 
+    // Get the character to be written 
+    char ch = 'G'; 
+  
+    // Write the Character to stdout 
+    putchar(ch); 
+  
+    return (0); 
+} `
