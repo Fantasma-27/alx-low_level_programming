fc8e17efdb5b6e3856a038e44ea88b74f16a8e94
fantasma-27 <camimarlyn@gmail.com>
2023-10-12 16:29:26 -0700

    corrections on mandatory 7-9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*main - prints the numbers from 1 to 100, followed by a new line but for multiples of three prints Fizz instead of the number and for the multiples of five prints Buzz
+*main - prnt no 1-100, then \n but for multiples 3 prnts Fizz, mutiples 5 buzz
 *Return: Always 0 (Success)
 */
 int main(void)
@@ -12,16 +12,20 @@ for (i = 1; i <= 100; i++)
 if (i % 3 == 0 && i % 5 != 0)
 {
 printf(" Fizz");
-} else if (i % 5 == 0 && i % 3 != 0)
+}
+else if (i % 5 == 0 && i % 3 != 0)
 {
 printf(" Buzz");
-} else if (i % 3 == 0 && i % 5 == 0)
+}
+else if (i % 3 == 0 && i % 5 == 0)
 {
 printf(" FizzBuzz");
-} else if (i == 1)
+}
+else if (i == 1)
 {
 printf("%d", i);
-} else
+}
+else
 {
 printf(" %d", i);
 }
