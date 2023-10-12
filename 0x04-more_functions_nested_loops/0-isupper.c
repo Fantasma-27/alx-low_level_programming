e9e2698486ee61be1170ff7fe5ea916c5c61fc4a
fantasma-27 <camimarlyn@gmail.com>
2023-10-12 22:18:07 +0300

    init

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,13 @@
 #include "main.h"
 /**
- *_isupper - uppercase letter
- *@c: char to check
- *Return:0 or 1
- */
+*_isupper - uppercase letter
+*@c: char to check
+*Return:0 or 1
+*/
 int _isuper(int c)
 {
-  if(c>='A' && c<='Z')
-    return (1);
-  else
-    return(0);
+if(c>='A' && c<='Z')
+return (1);
+else
+return(0);
 }
-	  
