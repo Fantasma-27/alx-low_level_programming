d494f5993660ff8b26afb21ef9cc2d43d8766814
fantasma-27 <camimarlyn@gmail.com>
2023-10-16 16:14:27 -0700

    betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,20 +8,20 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-  int l = 0;
-  int x = 0;
+	int l = 0;
+	int x = 0;
 
-  while (*(src + l) != '\0')
-    {
-      l++;
-    }
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
 
-  for (x = 0; x < l; x++)
-    {
-      dest[x] = src[x];
-    }
+	for (x = 0; x < l; x++)
+	{
+		dest[x] = src[x];
+	}
 
-  dest[l] = '\0';
+	dest[l] = '\0';
 
-  return dest;
+	return (dest);
 }
