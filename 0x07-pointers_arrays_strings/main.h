a1a47421e685171323be21720d7da7bfe6800d44
fantasma-27 <camimarlyn@gmail.com>
2023-10-25 01:36:42 -0700

    task 0-even more pointers

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,33 +1,9 @@
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  char board[8][8] = {
-		      {'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-		      {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-		      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-		      {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-		      {'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-  };
-  print_chessboard(board);
-  return (0);
-}#ifndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
-/**
- * void prototypes()
- * int prototypes()
- */
-
-int _putchar(char);
+#include <stdio.h>
+#include <stdlib.h>
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
@@ -38,4 +14,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /MAIN_H/
+#endif /*MAIN_H*/
