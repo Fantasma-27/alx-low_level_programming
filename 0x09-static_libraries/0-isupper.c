6ccce8295e9fb880ebb617457711ffbf227aae26
fantasma-27 <camimarlyn@gmail.com>
2023-11-20 06:02:57 -0800

    static lib

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _isupper - identify uppercase letters
- * @c: character that is being checked
- * Return: Either  0 or 1
- */
+*_isupper - uppercase letter
+*@c: char to check
+*Return:0 or 1
+*/
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-else
-return (0);
+        if (c >= 'A' && c <= 'Z')
+        return (1);
+        else
+        return (0);
 }
